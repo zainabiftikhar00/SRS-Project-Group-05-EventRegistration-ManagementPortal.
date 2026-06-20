834e0b3699377ed1e0669a7d3967a98fd2fd9010
zainabiftikhar00 <zainabiftikhar2106@gmail.com>
2026-06-21 01:49:46 +0500

    Created register page UI

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,81 @@
 function Register() {
-  return <h2>Register Page</h2>;
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+
+          <div className="card shadow">
+            <div className="card-body">
+
+              <h2 className="text-center mb-4">
+                Register
+              </h2>
+
+              <form>
+
+                <div className="mb-3">
+                  <label className="form-label">
+                    Full Name
+                  </label>
+
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter your name"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label">
+                    Email
+                  </label>
+
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter your email"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label">
+                    Password
+                  </label>
+
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label">
+                    Role
+                  </label>
+
+                  <select className="form-select">
+                    <option>Attendee</option>
+                    <option>Organizer</option>
+                  </select>
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn btn-primary w-100"
+                >
+                  Register
+                </button>
+
+              </form>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
