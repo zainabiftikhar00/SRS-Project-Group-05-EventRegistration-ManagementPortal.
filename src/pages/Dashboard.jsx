867a4c17138b141d474691ef27af91bf6f98a313
zainabiftikhar00 <zainabiftikhar2106@gmail.com>
2026-06-21 01:37:08 +0500

    Created events page with cards

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h2>Dashboard Page</h2>;
+}
+
+export default Dashboard;
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,49 @@
 function Events() {
+
+  const events = [
+    {
+      id: 1,
+      title: "Tech Conference 2026",
+      venue: "Islamabad",
+      date: "25 June 2026"
+    },
+    {
+      id: 2,
+      title: "AI Workshop",
+      venue: "Rawalpindi",
+      date: "30 June 2026"
+    },
+    {
+      id: 3,
+      title: "Sports Gala",
+      venue: "NUST",
+      date: "10 July 2026"
+    }
+  ];
+
   return (
-    <div>
-      <h2>Events Page</h2>
+    <div style={{ padding: "20px" }}>
+
+      <h1>Upcoming Events</h1>
+
+      {events.map((event) => (
+        <div
+          key={event.id}
+          style={{
+            border: "1px solid gray",
+            padding: "15px",
+            marginBottom: "15px",
+            borderRadius: "10px"
+          }}
+        >
+          <h3>{event.title}</h3>
+          <p>Venue: {event.venue}</p>
+          <p>Date: {event.date}</p>
+
+          <button>Register</button>
+        </div>
+      ))}
+
     </div>
   );
 }
