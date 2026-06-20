dcd8f904db8d30b1bea5c5a7e6a5663f543705ba
zainabiftikhar00 <zainabiftikhar2106@gmail.com>
2026-06-21 03:47:47 +0500

    Fixed login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,62 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import EventDetails from "./pages/EventDetails";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
 
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/events/:id" element={<EventDetails />} />
-      <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+return (
+
+<BrowserRouter>
+
+<Navbar />
+
+<Routes>
+
+<Route
+path="/"
+element={<Home />}
+/>
+
+<Route
+path="/events"
+element={<Events />}
+/>
+
+<Route
+path="/events/:id"
+element={<EventDetails />}
+/>
+
+<Route
+path="/register"
+element={<Register />}
+/>
+
+<Route
+path="/login"
+element={<Login />}
+/>
+
+<Route
+path="/dashboard"
+element={<Dashboard />}
+/>
+
+</Routes>
+
+</BrowserRouter>
+
+);
+
 }
 
 export default App;
